1d1997b25d59b7e6e0441368cf663b3631415126
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-11 03:54:35 -0400

    added input prompt

--- frontend.py
+++ frontend.py
@@ -1,13 +1,20 @@
 #/codespace/key-llme.py
-
 import torch
+from datetime import datetime
 from diffusers import DiffusionPipeline
 
-def main(model_id=str, image_prompt=str):
-    pipeline = DiffusionPipeline.from_pretrained(model_id).to("cuda")
+def main(model_base=str):
+    timestamp = datetime.now()
+    file_name = timestamp.strftime("%d-%m-%Y-%H-%M-%S")
+
+    image_prompt = input(">>> ")
+
+    model_base = "stabilityai/stable-diffusion-xl-base-1.0"
+    pipeline = DiffusionPipeline.from_pretrained(model_base).to("cuda")
     pipeline.load_lora_weights("nerijs/pixel-art-xl", weight_name="pixel-art-xl.safetensors")
+
     rendered_image = pipeline(image_prompt).images[0]
-    rendered_image.save("final_image.png")
+    rendered_image.save(f"{file_name}")
 
 if __name__ == "__main__":
-    main("stabilityai/stable-diffusion-xl-base-1.0", "a small rock with sunglasses on")
+    main()
