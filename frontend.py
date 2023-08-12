43c412cea31713be97ca2eaf32fd8d2249d09b62
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-12 16:35:04 -0400

    10x faster

--- frontend.py
+++ frontend.py
@@ -5,16 +5,20 @@ from diffusers import DiffusionPipeline
 
 def main():
     timestamp = datetime.now()
-    file_name = timestamp.strftime("%d-%m-%Y-%H-%M-%S")
-
-    image_prompt = input(">>> ")
+    file_name = timestamp.strftime("%d-%m-%Y-%H-%M")
 
     model_base = "stabilityai/stable-diffusion-xl-base-1.0"
-    pipeline = DiffusionPipeline.from_pretrained(model_base).to("cuda")
-    pipeline.load_lora_weights("nerijs/pixel-art-xl", weight_name="pixel-art-xl.safetensors")
+    lora_home = "nerijs/pixel-art-xl"
+    sf_weights = "pixel-art-xl.safetensors"
 
-    rendered_image = pipeline(image_prompt).images[0]
-    rendered_image.save(f"{file_name}.png")
+    pipeline = DiffusionPipeline.from_pretrained(model_base, torch_dtype=torch.float16, use_safetensors=True, local_files_only=True, safety_check=None).to("cuda")
+    pipeline.load_lora_weights(lora_home, weight_name=sf_weights)
+
+    user_prompt = input("key-llme-py>>> ")
+    with torch.no_grad():
+        _image = pipeline(user_prompt).images[0]
+        
+    _image.save(f"{file_name}.png")
 
 if __name__ == "__main__":
     main()
