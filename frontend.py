bbccabe80bba9fd9be333f989c4f2375f2969750
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-12 17:26:36 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 #/codespace/key-llme.py
 import torch
 from datetime import datetime
-from diffusers import DiffusionPipeline
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 
 def main():
     timestamp = datetime.now()
@@ -12,12 +12,14 @@ def main():
     sf_weights = "pixel-art-xl.safetensors"
 
     pipeline = DiffusionPipeline.from_pretrained(model_base, torch_dtype=torch.float16, use_safetensors=True, local_files_only=True, safety_check=None).to("cuda")
+    pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
     pipeline.load_lora_weights(lora_home, weight_name=sf_weights)
 
     user_prompt = input("key-llme-py>>> ")
     with torch.no_grad():
-        _image = pipeline(user_prompt).images[0]
-        
+        _image_seed = torch.Generator("cuda").manual_seed(0)
+        _image = pipeline(user_prompt, generator=_image_seed, num_inference_steps=50).images[0]
+
     _image.save(f"{file_name}.png")
 
 if __name__ == "__main__":
