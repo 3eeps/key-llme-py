d5b67823304badeb31b587a6f81db117c0ff5278
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-26 10:32:48 -0400

    fixed some ui stuff

--- frontend.py
+++ frontend.py
@@ -5,35 +5,37 @@ from datetime import datetime
 from diffusers import DiffusionPipeline
 
 def main():
-    time_stamp = datetime.now()
-    output_file_name = time_stamp.strftime("%d-%m-%Y-%H-%M")
-    
     model_path = "stabilityai/stable-diffusion-xl-base-1.0"
-    lora_path = "/codespace/loras"
+    lora_path = "./loras"
+    lora_list = []
     file_list = os.scandir(lora_path)
     
-    print("# load lora")
-    iter = 0
-    lora_list = []
+    print(model_path)
+    print(lora_path)
+    list_num = 0
     for obj in file_list:
-        iter = iter + 1
+        list_num = list_num + 1
         if obj.is_file():
-            print(f"{iter}.) {obj.name}")
+            file_name = lora_path + "/" + obj.name
+            file_stats = os.stat(file_name)
+            print(f"{list_num}.) {obj.name} - {int(file_stats.st_size / 1048576)}mb")
             lora_list.append(obj.name)
     file_list.close()
-    lora_num = int(input("#>>> "))
+
+    lora_num = int(input(">>> "))
     lora_to_load = lora_list[lora_num - 1]
-    print(f"sdxl 1.0 + {lora_to_load}")
+    print(f"loading sdxl 1.0 + {lora_to_load}")
 
     pipeline = DiffusionPipeline.from_pretrained(pretrained_model_name_or_path=model_path, torch_dtype=torch.float16)
     pipeline.enable_model_cpu_offload()
     pipeline.load_lora_weights(pretrained_model_name_or_path_or_dict=lora_path, weight_name=lora_to_load)
 
     while True:
-        user_prompt = input("%>>> ")
-        
-        _image = pipeline(prompt=user_prompt).images[0]
+        user_prompt = input(">>> ") 
+
+        output_file_name = datetime.now().strftime("%D-%M-%Y-%H-%M-%S")
+        _image = pipeline(prompt=user_prompt).images[0]      
         _image.save(f"{output_file_name}.png")
 
 if __name__ == "__main__":
-    main() 
+    main()
